79c5136a1c78772ff071db85e67bc8415d8e9180
Administrator <administrator@Tomaszs-MacBook-Air.local>
2016-10-08 01:01:28 +0300

    4 commit

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <html>
-<head></head>
+<head>
+  <meta charset="UTF-8">
+  <title>Задание три.</title>
+</head>
 <body>
+  <a href="http://localhost:8000/index.php?name=George">ссылка</a>
 <p></p>
 <?php
-
-echo "Privet, Dima!!!";
+if (!empty($_GET["name"])){
+  echo "Privet, " . $_GET["name"] . "!!!";
+}
+else {
+  echo "Error!!!!";
+}
 
  ?>
  </body>
