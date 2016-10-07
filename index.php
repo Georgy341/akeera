d728eb119ae9268a77682e519d30d98c95a9af49
Administrator <administrator@Tomaszs-MacBook-Air.local>
2016-10-08 01:17:35 +0300

    5 commit

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Задание три.</title>
+  <title>Задание четыре.</title>
 </head>
 <body>
-  <a href="http://localhost:8000/index.php?name=George">ссылка</a>
-<p></p>
+  <form method="post" action="index.php">Заполни поле:<br>
+    <br>
+    Укажите ваше имя: <input name="user_name" type="text" maxlength="20" size="25"
+    value="" />
+    <br><br> <input type=submit value="Передать информацию"></form>
+
 <?php
-if (!empty($_GET["name"])){
-  echo "Privet, " . $_GET["name"] . "!!!";
+if (!empty($_POST["user_name"])){
+  echo "Privet, " . $_POST["user_name"] . "!!!";
 }
 else {
   echo "Error!!!!";
