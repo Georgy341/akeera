2bccf2a91a5e51243ac584b336ee61b95650ed57
George <Georgy.a.341@gmail.com>
2016-10-17 23:08:38 +0300

    Update AutoPostVK.php

--- AutoPostVK.php
+++ AutoPostVK.php
@@ -2,7 +2,7 @@
 include __DIR__ . '/vendor/autoload.php';
 
 //The constructor
-$vk = new Vk("66bfdcd2bbb6223dcf3e816b7b35b6f606cbad2d0b4e6d17527fe7d67e2b10753a641eaa43d7140d64b8a");
+$vk = new Vk("66bfdcd2bbb6223dcf3e816b7b35b6f606cbad2d0b4e6d17527fe7d67e2b10753a6417140d64b8a");
 
 //Set user online
 $vk->method('account.setOnline');
